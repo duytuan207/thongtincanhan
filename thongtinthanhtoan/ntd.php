1fdeb27ee2d2076d964feecc8caffce0d78e43ca
duytuan207 <88073690+duytuan207@users.noreply.github.com>
2022-08-27 20:38:05 +0700

    Update ntd.php

--- thongtinthanhtoan/ntd.php
+++ thongtinthanhtoan/ntd.php
@@ -5,19 +5,19 @@ $ntdbanner = 'https://i.imgur.com/XafrUBY.png'; // banner của hình
 /* ------------------------------------- */
 $ntdicon = 'https://i.imgur.com/VTHXeSL.jpg'; // thay shortcuticon và icon
 /* ------------------------------------- */
-$ten = 'LÊ GIA HƯNG'; // ghi full tên bằng in hoa
+$ten = 'Nguyễn Duy Tuấn'; // ghi full tên bằng in hoa
 /* ------------------------------------- */
-$tieusu = 'Hưng Kòy'; // tên tiểu sử
+$tieusu = 'Iris'; // tên tiểu sử
 /* ------------------------------------- */
 $tt = 'Ngày Sinh : 22/10/2007 <br>Quê Quán : Nho Quan , Ninh Bình <br>SDT : 0972260366 <br>Trạng Thái : Có 10 Vợ <br>Hiện tại mình vừa học vừa làm cũng như phát triển bản thân để ngày một hoàn thiện hơn . Đối với mình mỗi ngày chỉ cần cố gắng hơn 1 chút thì tương lai chắc chắn sẽ tốt hơn'; //muốn xuống dòng thì bên <br>
 /* ------------------------------------- */
-$tkmbbank ='4510102006'; //nhập tk mbbank
-$tenmbbank ='LÊ GIA HƯNG'; //tên mbbank
+$tkmbbank ='16001012062081'; //nhập tk mbbank
+$tenmbbank ='NGUYEN DUY TUAN-MSB BANK'; //tên mbbank
 /* ------------------------------------- */
 $tkmomo ='0972260366'; //nhập tk momo
-$tenmomo = 'LÊ GIA HƯNG'; // tên momo
+$tenmomo = 'NGUYỄN DUY TUẤN'; // tên momo
 /* ------------------------------------- */
-$idfb ='koy206'; // nhập id fb vào
+$idfb ='ngtuan.profile'; // nhập id fb vào
 /* ------------------------------------- */
-$tencopyright ='Hưng Kòy'; //tên ở dòng coppyright
+$tencopyright ='NGUYỄN DUY TUẤN'; //tên ở dòng coppyright
 ?>
